557f420342f269f0da6af68d30b6f1965d536dd9
yashdev9274 <yashdev.yvd@gmail.com>
2024-12-06 19:33:39 +0530

    init FastSearch

---
+++ src/lib/openai.ts
@@ -0,0 +1,10 @@
+import OpenAI from 'openai'
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+// import { ChatOpenAI } from "langchain/chat_models/openai";
+
+// const llm = new ChatOpenAI();
+// // await llm.invoke("Hello, world!");
